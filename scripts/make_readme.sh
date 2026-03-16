23a0bff30c54e304ec582cca513058721bfa5f93
s-shimizu-trinet <s.shimizu@trinet.co.jp>
2026-03-16 19:21:45 +0900

    feat: implement release note generation

--- scripts/make_readme.sh
+++ scripts/make_readme.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 # bash make_readme.sh
 
-target_directory=data
+# target_directory=data
+target_directory=../test_data
+
 tmp_directory=tmp
-tmp_output_file_path=${tmp_directory}/latest_release_note.csv
-config_directory=configs
+tmp_output_file_path=${tmp_directory}/release_note.csv
+
+# config_directory=configs
+config_directory=../configs
 
 source ./scripts/make_readme_functions.sh
 
-
-if [ `ls ${target_directory}/latest_release_note.txt > /dev/null; echo $?` -ne 0 ]; then
-    old_releasenote_date=`date -r "${target_directory}/latest_release_note.txt" +%Y%m%d`
-    mkdir ${target_directory}/release_notes
-    cp "${target_directory}/latest_release_note.txt" "${target_directory}/relese_notes/${old_releasenote_date}.txt"
-    rm "${target_directory}/latest_release_note.txt"
-fi
-
-mkdir ${tmp_directory}
-echo "データ名,バージョン/最終取得日,内容,作成者" >> ${tmp_output_file_path}
+mkdir -p "${tmp_directory}"
+echo "file,datasource,update" > "${tmp_output_file_path}"
 
 # nandoの更新日(サーバーから取得)
 # file_nameを抽出 > サフィックスを切り捨て > データ名に変換
-files=($(cat ${config_directory}/copy_file_list.csv | awk -F ',' '{print $1}' | awk -F '.' '{print $1}' | sort | uniq | tr '\n' ' '))
 read_version_from_server
+
+create_graph_version_list
 
 # APIから取得するデータの更新日
 config_file_path="${config_directory}/api_list.csv"
 read_version_from_api
 
 # 整形
-output_file_path=`echo ${tmp_output_file_path} | sed 's/.csv/.txt/g'`
-printf '# Release Note\n\n' > ${output_file_path}
-duckdb -markdown -c "SELECT * from read_csv('${tmp_output_file_path}')" >> ${output_file_path}
+output_file_path="${tmp_output_file_path%.csv}.txt"
+printf '# Release Note\n\n' > "${output_file_path}"
+duckdb -markdown -c "SELECT * FROM read_csv('${tmp_output_file_path}', header=true)" >> "${output_file_path}"
 
 # latestに移行
-cp "tmp/latest_release_note.txt" "${target_directory}/latest_release_note.txt"
+latest_dir=$(readlink "${target_directory}/latest")
+latest_path="${target_directory}/${latest_dir#./}"
+release_target="${latest_path}/RELEASE.txt"
+
+cp "${output_file_path}" "${release_target}"
 
 echo "done"
 
-rm -rf tmp
+rm -rf "${tmp_directory}"
--- scripts/make_readme_functions.sh
+++ scripts/make_readme_functions.sh
@@ -1,51 +1,102 @@
+#!/bin/bash
+
+create_graph_version_list () {
+
+    # local config_file="${config_directory}/graph_source.csv"
+    local config_file="${config_directory}/graph_source_local.csv"
+    local output_file="${tmp_directory}/graph_version.tsv"
+
+    mkdir -p "${tmp_directory}"
+
+    echo -e "graph\tdatabase\tversion" > "${output_file}"
+
+    awk -F',' 'NR>1 {print $1 "|" $2 "|" $3 "|" $4}' "$config_file" | \
+    while IFS="|" read -r api version_source database format
+    do
+        api=$(echo "$api" | tr -d '\r')
+        version_source=$(echo "$version_source" | tr -d '\r')
+        database=$(echo "$database" | tr -d '\r')
+        format=$(echo "$format" | tr -d '\r')
+
+        version="APIで取得"
+
+        if [[ "$format" != "API" && -n "$version_source" && -f "$version_source" ]]; then
+
+            line=$(grep -E "$format" "$version_source" | head -n 1)
+
+            date=$(echo "$line" | grep -oE "[0-9]{4}-[0-9]{2}-[0-9]{2}" | head -n 1)
+
+            if [ -n "$date" ]; then
+                version=$(echo "$date" | sed 's/-/\//g')
+            fi
+        fi
+
+        echo -e "${api}\t${database}\t${version}" >> "${output_file}"
+
+    done
+}
+
 read_version_from_server () {
-    local files=${files[@]}
-    local target_directory=${target_directory}
-    local output=${tmp_output_file_path}
-    local update_date=''
 
-    for file in ${files[@]}; do
+    local output="${tmp_output_file_path}"
+    local latest_dir="${target_directory}/latest"
 
-        # headerはスキップ
-        if [ "${file}" = "file_name_with_suffix" ]; then
+    for file_path in "${latest_dir}"/*
+    do
+        file_name=$(basename "$file_path")
+
+        if [[ "$file_name" == "RELEASE.txt" || "$file_name" == "error.log" ]]; then
             continue
         fi
 
-        # データソースは同じで拡張子だけが違うファイルから適当に1つ選んで更新日を入れているので、どれくらい精密にするか要検討
-        file_name=`ls ${target_directory} | grep ${file} | sed -n 1p`
-        update_date=`date -r "${target_directory}/${file_name}" +%Y/%m/%d`
-        echo "${file},${update_date},,'ライフサイエンス統合データベースセンター'" >> ${output}
+        version="APIで取得"
+        datasource="Nanbyo Disease Ontology"
+
+        if [[ -f "$file_path" && ( "$file_name" == *.ttl || "$file_name" == *.rdf ) ]]; then
+
+            line=$(grep -E "versionIRI|versionInfo" "$file_path" | head -n 1)
+
+            date=$(echo "$line" | grep -oE "[0-9]{4}-[0-9]{2}-[0-9]{2}" | head -n 1)
+
+            if [ -n "$date" ]; then
+                version=$(echo "$date" | sed 's/-/\//g')
+            fi
+        fi
+
+        echo "${file_name},${datasource},${version}" >> "${output}"
+
     done
 }
 
 read_version_from_api () {
-    local config_file_path=${config_file_path}
-    local target_directory=${target_directory}
+    local config_file_path="${config_file_path}"
     local output=${tmp_output_file_path}
-    local update_date=''
+    local graph_version_file="${tmp_directory}/graph_version.tsv"
 
-    while IFS=, read -r file_name api version; do
+    while IFS=, read -r file_name api from; do
 
 	# ヘッダー行をスキップ
     	if [ "$file_name" = "output" ]; then
             continue
     	fi
 
-        # latestディレクトリを参照
-        latest_dir="${target_directory}/latest"
+        IFS='|' read -ra sources <<< "$from"
 
-        if [ -f "${latest_dir}/${file_name}.json" ]; then
-            update_date=$(date -r "${latest_dir}/${file_name}.json" +%Y/%m/%d)
-        elif [ -f "${latest_dir}/${file_name}.txt" ]; then
-            update_date=$(date -r "${latest_dir}/${file_name}.txt" +%Y/%m/%d)
-        else
-            update_date=""
-        fi
+        for src in "${sources[@]}"; do
 
-        echo "${file_name},${update_date},,${version}" >> "${output}"
+            row=$(awk -F'\t' -v graph="$src" '$1==graph{print;exit}' "$graph_version_file")
 
-            # TODO: 間にAPIからバージョン取得する処理が必要
-            # TODO: versionをとるAPIじゃなくてソースのURLを受け取る処理が必要？
+            database=$(echo "$row" | awk -F'\t' '{print $2}')
 
-    done < ${config_file_path}
+            version=$(echo "$row" | awk -F'\t' '{print $3}')
+
+            if [ -z "$version" ]; then
+                version="APIで取得"
+            fi
+
+            echo "${file_name}.txt,${database},${version}" >> "${output}"
+
+        done
+
+    done < "${config_file_path}"
 }
